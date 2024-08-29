e3d23345d3d155247bbf24a5e960920a298667a1
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2024-08-29 18:25:45 -0500

    Most Recently Grabbed Element Is Brought To Front

--- app.js
+++ app.js
@@ -5,47 +5,53 @@
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-  for (const Header of document.getElementsByClassName('header')) {
-    const Parent = Header.parentElement;
+  let editors_index_z = 1;
+  for (const header of document.getElementsByClassName('header')) {
+    const parent = header.parentElement;
 
-    let isDragging = false;
+    let is_dragging = false;
     let initial_x, initial_y, initial_left, initial_top;
+
+    if(parent.style.zIndex > editors_index_z) {
+      editors_index_z = editors_index_z;
+    }     
   
     const onMouseMove = (Event) => {
-      if (!isDragging) return;
+      if (!is_dragging) return;
   
       const delta_x = Event.clientX - initial_x;
       const delta_y = Event.clientY - initial_y;
   
       requestAnimationFrame(() => {
-        Parent.style.left = `${initial_left + delta_x}px`;
-        Parent.style.top = `${initial_top + delta_y}px`;
+        parent.style.left = `${initial_left + delta_x}px`;
+        parent.style.top = `${initial_top + delta_y}px`;
+        parent.style.zIndex = editors_index_z++;
       });
     };
-  
-    Header.addEventListener('mousedown', (Event) => {
-      isDragging = true;
-      initial_x = Event.clientX;
-      initial_y = Event.clientY;
-  
-      const rect = Header.getBoundingClientRect();
-      initial_left = rect.left;
-      initial_top = rect.top;
-  
-      Header.style.cursor = 'grabbing';
-  
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
-    });
-  
+
     const onMouseUp = () => {
-      if (!isDragging) return;
+      if (!is_dragging) return;
   
-      isDragging = false;
-      Header.style.cursor = 'grab';
+      is_dragging = false;
+      header.style.cursor = 'grab';
   
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
     };
+  
+    header.addEventListener('mousedown', (Event) => {
+      is_dragging = true;
+      initial_x = Event.clientX;
+      initial_y = Event.clientY;
+  
+      const rect = header.getBoundingClientRect();
+      initial_left = rect.left;
+      initial_top = rect.top;
+  
+      header.style.cursor = 'grabbing';
+  
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
   }
 });
